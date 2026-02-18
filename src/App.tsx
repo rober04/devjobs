1b8c8cbc1e0924d2da8a58cd7208c8c65c72a0b0
Rober04 <roberto.david.adam04@gmail.com>
2026-02-18 13:50:53 +0100

    feat: add routing to Home, Search, Jobs, and Companies pages in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import { Header } from "@/components/Header";
+import { Route } from "./components/Route";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Route
+        path="/"
+        component={() => <h2 style={{ color: "white" }}>Home Page</h2>}
+      />
+      <Route
+        path="/search"
+        component={() => <h2 style={{ color: "white" }}>Search Page</h2>}
+      />
+      <Route
+        path="/jobs"
+        component={() => <h2 style={{ color: "white" }}>Jobs Page</h2>}
+      />
+      <Route
+        path="/companies"
+        component={() => <h2 style={{ color: "white" }}>Companies Page</h2>}
+      />
+    </>
+  );
 }
 
 export default App;
