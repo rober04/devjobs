e692b695ab8640c6a61135304762d4b3ef0b9959
Rober04 <roberto.david.adam04@gmail.com>
2026-02-19 13:48:09 +0100

    feat: created footer with its corresponding styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "@/styles/Footer.css";
+
+export function Footer() {
+  return (
+    <footer className="footer">© 2026 DevJobs. All rights reserved.</footer>
+  );
+}
+
+export default Footer;
