4416fd23e7a9a8b70377a545ec945740d0aa078c
Rober04 <roberto.david.adam04@gmail.com>
2026-02-19 02:23:52 +0100

    feat: added banner component with non-functional search bar

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,8 +1,14 @@
+import "@/styles/Home/Banner.css";
+
 export function Banner() {
   return (
-    <>
-      <h1>BANNER</h1>
-    </>
+    <section className="banner">
+      <h1 className="banner__title">Find the job of your dreams</h1>
+      <h3 className="banner__subtitle">
+        Join the biggest community of developers and find your next opportunity.
+      </h3>
+      <HomeSearchBar />
+    </section>
   );
 }
 
